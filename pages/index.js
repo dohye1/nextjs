9ee2ba26f4f44a46f68bd9ca256596df6e79b593
hyewon <tsc03018@gmail.com>
2021-03-28 22:10:01 +0900

    nextjs사용해보기(기초)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-const Index = () => {
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const ProfileLink = (props) => {
   return (
     <div>
-      <p>index page</p>
+      <Link href='/p/[profile]' as={`/p/${props.profile}`}>
+        <a>Go to {props.name}'s profile</a>
+      </Link>
     </div>
   );
 };
+const Index = () => {
+  return (
+    <Layout>
+      <h1>Friends List</h1>
+    </Layout>
+  );
+};
 
+// 여기서부터 하면됨 : 무료 api 찾기
+/*Index.getInitialProps = async function () {
+  const res = await fetch('https://uinames.com/api/?amount=10');
+  const data = await res.json();
+
+  return {
+    profiles: data.map((profile) => profile.name)
+  };
+};*/
 export default Index;
